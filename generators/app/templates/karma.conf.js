6f01cdadf4e113015f8f59983e71f82c7f97cfee
David Laughlin <davidclaughlin@gmail.com>
2017-11-10 02:15:00 -0800

    Up and Running Before Composibility With User Interaction and Basic Templating

---
+++ generators/app/templates/karma.conf.js
@@ -0,0 +1,89 @@
+//documentation on this trick for integrating webpack and karma: http://mike-ward.net/2015/09/07/tips-on-setting-up-karma-testing-with-webpack/
+var webpackConfig = require('./webpack.config.js');
+
+
+// Karma configuration
+// Generated on Mon Oct 16 2017 15:23:30 GMT-0700 (Pacific Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['<%= props.karmaFrameworks %>' /*'jasmine'*/],
+    
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        './src/**.spec.js',
+    ],
+    
+    // list of files to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        './src/feature.controller.spec.js' : ['webpack'],
+        //'./test/**/*.spec.js': ['webpack'],
+        //'./test/*.spec.js': ['webpack'],
+        //'./test/**/*.mock.js': ['webpack'],
+        //'./test/*.mock.js': ['webpack'],
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+        noInfo: true,
+        stats: {
+            chunks: false
+        }
+    },
+
+    plugins: [
+        'karma-webpack',
+        'karma-jasmine',
+        'karma-chrome-launcher'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    //reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    //disabled to allow Grunt to run it as a child process
+    //autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    //singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
---
+++ generators/app/templates/src/feature.controller.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+import { app } from './app.module.js';
+import './feature.service.js';
+
+export default app.controller('featureController', FeatureController);
+
+FeatureController.$inject = ['$location'
+                            , 'featureService'];
+function FeatureController($location
+                            , featureService) {
+    var vm = this;
+    vm.title = 'feature';
+    vm.text = null;
+
+    _activate();
+    //////////////////////////////////////
+
+    function _activate() {
+        vm.text = featureService.getText();
+    }
+};
